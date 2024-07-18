1e003d74fc2be12214b9eb063f6460c5524b028a
Cédric <levycedric@hotmail.fr>
2024-07-18 16:45:48 +0200

    test route

--- models/tweets.js
+++ models/tweets.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 const tweetSchema = mongoose.Schema({
   tweet: String,
-  hashtag:[String],
-  nbLike:Number,
-  date:Date
+  hashtag:[String ],
+  date:Date,
+  nbLike:[{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],     // c'est les users qui ont liké le tweet //
+                                                                      // On met un tableau  car plusieurs users qui likent 
+  user:{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }        // c'est le user qui a ecrit le tweet
 
 });
 
 const Tweet = mongoose.model('tweets', tweetSchema);
-
 
 module.exports = Tweet;
--- routes/tweets.js
+++ routes/tweets.js
@@ -6,13 +6,7 @@ const Tweet = require('../models/tweets');
 
 
 
-
-
-
-
-
-
-router.get('/',(req, res,)=> {
+router.get('/',(req, res,)=> {    // route tweet en get pour récupérer les infos de la base de données sur l'ensemble des tweets.
  Tweet.find()
 .then(data => {
 res.json({tweet:data})
@@ -20,8 +14,8 @@ res.json({tweet:data})
 )})
 
 
-router.post('/', (req, res) => {  //   / on ecrit pas le nom de la route car elle est déja intégrée dans le app.js
-const newTweet = new Tweet({
+router.post('/', (req, res) => {  //                              / on écrit pas le nom de la route car elle est déja intégrée dans le app.js
+const newTweet = new Tweet({                                     // route post pour ajouter des nouveaux tweets en base de donnée.
   tweet:req.body.tweet
 })
 
@@ -36,8 +30,37 @@ const newTweet = new Tweet({
    
 });
 
+router.delete('/:id',(req,res)=>{                            // id en param (dans thunder Client écrire un vrai id pour tester)
+             
+    Tweet.deleteOne({_id:req.params.id }).then(() => {      // supprimer l'id qui est égal à 'id de la requête . c'est l'id qui correspond au bouton supprimer
+
+        Tweet.find().then(data => {
+          res.json({result: true});                           // res.json pour avoir un retour conernant notre requête
+        });
+       
+       });
 
 
 
+})
+
+/*router.put('/', (req, res) => { 
+
+    const counter = new Counter({
+        tweet:req.body.tweet
+      })
+        newTweet.save().then(()=> {
+      
+          Tweet.find().then(data => {
+      
+              res.json({test:data});
+            });
+          });
+         
+     });
+
+ajouter l'id dans la propriete numberlike de tweet*/
+
+    
 
 module.exports = router;
