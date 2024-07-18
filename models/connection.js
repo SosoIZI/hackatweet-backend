91dc81ef65da4a7ff9639377c0ff59f5f07d7d53
Cédric <levycedric@hotmail.fr>
2024-07-18 11:54:31 +0200

    test

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString = "mongodb+srv://ClusterSoso:wtmNFlLOkcKVEHli@clustersoso.7lda9ao.mongodb.net/hackatweet"
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
--- models/users.js
+++ models/users.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
+  firstname: String,
   username: String,
   password: String,
   token: String,
+  tweet: { type: mongoose.Schema.Types.ObjectId, ref: 'tweets' },
+
 });
 
 const User = mongoose.model('users', userSchema);
 
 module.exports = User;
+
+
