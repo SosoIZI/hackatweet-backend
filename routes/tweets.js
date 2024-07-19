8cc73223d3b6389a0bb99e0cd1f3801c1e2aa698
Emma <emma.lenfant@icloud.com>
2024-07-19 14:23:29 +0200

    ok

--- routes/tweets.js
+++ routes/tweets.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 require('../models/connection');
 const Tweet = require('../models/tweets');
+const User = require('../models/users');
 
 
 router.get('/',(req, res,)=> {    // route tweet en get pour récupérer les infos de la base de données sur l'ensemble des tweets.
@@ -38,22 +39,35 @@ router.delete('/:id',(req,res)=>{                            // id en param (dan
        });
 })
 
-// router.put('/:token/:id', (req, res) => { 
-//     Tweet.findOne({ _id: req.params.id }).then(data => {
-//         if (data) {
-//     User.findOne({token:req.params.token})
-// .then(data => {
+// route PUT => mise à jour du compteur 
 
-// console.log()
-// //Post.findOneAndUpdate(
-//            // { _id: ObjectId(req.params.id) },
-//            // { $push: { nbLike: req.user._id },
-//           }
+// router.put('/:token/:id', (req, res) => {      
+//     User.findOne({token:req.params.token})                       // on trouve le document user en fonction de son token 
+// .then(data => {
+//   if(data) {
+//     try {                                                        // try...catch =>  regroupe des instructions à exécuter
+//       Tweet.findOne({ _id: req.params._id }).then(data => {      // on trouve le document tweet en fonction de son id
+//         if (data && !nbLike.includes(req.user._id))              // si le tableau de like ne contient pas le 
+//         // console.log(re.user._id)
+//           {
+//           Post.findOneAndUpdate(
+//             { nbLike: ObjectId(req.params.id) },
+//             { $push: { nbLike: req.user._id.length } },
+//             { "new": true, "upsert": true}
 //           );
 //         }
-    
+//         else {
+          
+//         }
+
+//       });
+
+//     } catch(err) {
+//   console.log('Error => ', err)
+//     }
+//   }
+//   }
+// );
 // })
-// })       
-    
-// user.find 
+
 module.exports = router;
